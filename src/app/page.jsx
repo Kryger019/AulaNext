a20de067720c8e79deabaf06f0f0ddca484f45f2
Kryger019 <160683097+Kryger019@users.noreply.github.com>
2024-07-31 20:06:17 -0300

    aulanext 31/07/24
    aulanext que concertei no dia 31-07-24

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className='container'>
+        <h1 className={styles.titulo}>Aula Next</h1>
+    </div>
+  );
+}
